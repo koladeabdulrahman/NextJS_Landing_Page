805ebe4e4aa4c8c466ad2d0559cd95c40b0b014b
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-05-27 14:58:32 +0100

    Updated Pricing.tsx to add keys to mappings

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -75,8 +75,9 @@ export const Pricing = () => {
               popular,
               inverse,
               features,
-            }) => (
+            }, index) => (
               <div
+                key = {index}
                 className={twMerge(
                   "card",
                   inverse === true && "border-black bg-black text-white"
@@ -127,8 +128,8 @@ export const Pricing = () => {
                   {buttonText}
                 </button>
                 <ul className="flex flex-col gap-5 mt-8">
-                  {features.map((feature) => (
-                    <li className="text-sm flex items-center gap-4">
+                  {features.map((feature, index) => (
+                    <li key = {index} className="text-sm flex items-center gap-4">
                       <CheckIcon className="h-6 w-6" />
                       <span>{feature}</span>
                     </li>
